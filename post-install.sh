1d61a0fd38e641045d0e54ecddbb2880c979ff16
Eduard <147715855+z-Eduard005@users.noreply.github.com>
2026-05-04 19:27:28 +0300

    feat: add error handling to whole script and media are pre-downloaded now

--- post-install.sh
+++ post-install.sh
@@ -9,9 +9,11 @@ RPM_FUSION_CMD="https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 EXT_CLI="$HOME/.local/bin/gnome-extensions-cli"
 WALLPAPERS_DIR="$HOME/.local/share/backgrounds"
 WALLPAPERS_URL="$RAW_GITHUB/wallpapers"
+WALLPAPER_FILENAMES=(windows.jpg macos.png linux.jpg)
 DNF_CONF="/etc/dnf/dnf.conf"
-SCRIPT_DATA_DIR="$HOME/.local/share/z-Eduard005-fedora-post-install"
+PROJECT_DIR="$HOME/Programs/fedora-post-install"
 LIBREOFFICE_USER_DIR="$HOME/.config/libreoffice/4/user"
+DTP_CONF_PATH="/org/gnome/shell/extensions/dash-to-panel/"
 
 REMOVE_PKGS=(gnome-tour baobab malcontent-control yelp)
 DNF_PKGS=(python3-pip zsh gnome-tweaks steam)
@@ -23,7 +25,23 @@ success() { printf "\033[1;32m%s\033[0m" "$1"; }
 err() { printf "\033[1;31m%s\033[0m" "$1"; }
 warn() { printf "\033[1;33m%s\033[0m" "$1"; }
 info() { printf "\033[1;34m%s\033[0m" "$1"; }
-throw_err() { echo "$(err "$1")"; exit 1; }
+throw_err() {
+  echo "$(err "$1")"
+  zenity --info \
+  --title="Error happend:" \
+  --text="$1" \
+  --width=960 --height=540
+  exit 1
+}
+
+ask_confirm() {
+  read -rp "$(warn "$1 [y/N]: ")" proceed
+  if [[ "$proceed" == [yY] ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 set_dnf_conf_option() {
   local key="$1"
@@ -60,8 +78,8 @@ while true; do
   kill -0 "$$" || exit
 done 2>/dev/null &
 
-mkdir -p "$SCRIPT_DATA_DIR"
-STATE_FILE="$SCRIPT_DATA_DIR/state"
+mkdir -p "$PROJECT_DIR"
+STATE_FILE="$PROJECT_DIR/state"
 [ ! -f "$STATE_FILE" ] && touch "$STATE_FILE"
 
 step="[1|13]: Configuring system package manager"
@@ -87,27 +105,30 @@ run_the_step && {
 
 step="[4|13]: Installing essential codecs"
 run_the_step && {
-  sudo dnf install -y "${CODEC_PKGS[@]}" --allowerasing
+  sudo dnf install -y "${CODEC_PKGS[@]}" --allowerasing || throw_err "Error while installing codecs"
 } && save_step
 
 step="[5|13]: Installing essential programs"
 run_the_step && {
-  sudo dnf install -y "${DNF_PKGS[@]}"
-  sudo flatpak install -y flathub "${FLATHUB_PKGS[@]}"
-  sudo flatpak install -y fedora "${FLATPAK_PKGS[@]}"
-  pip3 install $(basename $EXT_CLI)
-  eval "$OMZ_INSTALLER"
+  (
+    set -e
+    sudo dnf install -y "${DNF_PKGS[@]}"
+    sudo flatpak install -y flathub "${FLATHUB_PKGS[@]}"
+    sudo flatpak install -y fedora "${FLATPAK_PKGS[@]}"
+    pip3 install $(basename $EXT_CLI)
+    [ ! -d "$HOME/.oh-my-zsh" ] && eval "$OMZ_INSTALLER"
+  ) || throw_err "Error while installing essential programs"
 } && save_step
 
 step="[6|13]: Removing unnecessary programs"
 run_the_step && {
-  sudo dnf remove -y "${REMOVE_PKGS[@]}"
+  sudo dnf remove -y "${REMOVE_PKGS[@]}" || throw_err "Error while removing unnecessary programs"
 } && save_step
 
 step="[7|13]: Make the system start faster"
 run_the_step && {
   sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=3/' /etc/default/grub
-  sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+  sudo grub2-mkconfig -o /boot/grub2/grub.cfg || throw_err "Error while generating grub config"
 } && save_step
 
 step="[8|13]: Tweaking terminal"
@@ -123,7 +144,7 @@ run_the_step && {
 
 step="[9|13]: Changing default music app"
 run_the_step && {
-  xdg-mime default com.github.neithern.g4music.desktop audio/mpeg audio/flac audio/x-wav audio/ogg
+  xdg-mime default com.github.neithern.g4music.desktop audio/mpeg audio/flac audio/x-wav audio/ogg || echo "$(warn "Failed to set default music app")"
 } && save_step
 
 step="[10|13]: Tweaking system settings"
@@ -139,9 +160,7 @@ run_the_step && {
   gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "['<Shift><Control><Alt>space']"
   gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>w']"
   gsettings set org.gnome.shell favorite-apps "['org.gnome.Ptyxis.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Settings.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'org.gnome.Software.desktop', 'org.gnome.TextEditor.desktop', 'org.gnome.SystemMonitor.desktop', 'org.mozilla.firefox.desktop', 'steam.desktop']"
-  if ! gsettings get org.gnome.desktop.input-sources xkb-options | grep -q "grp:caps_toggle"; then
-    gsettings set org.gnome.desktop.input-sources xkb-options "['grp:caps_toggle','lv3:ralt_switch']"
-  fi
+  gsettings set org.gnome.desktop.input-sources xkb-options "['grp:caps_toggle','lv3:ralt_switch']"
 
   mkdir -p "$LIBREOFFICE_USER_DIR"
   cat > "$LIBREOFFICE_USER_DIR/registrymodifications.xcu" << 'EOF'
@@ -158,9 +177,16 @@ EOF
 
 step="[11|13]: Installing essential gnome extensions"
 run_the_step && {
-  $EXT_CLI install appindicatorsupport@rgcjonas.gmail.com quick-lang-switch@ankostis.gmail.com blur-my-shell@aunetx just-perfection-desktop@just-perfection Vitals@CoreCoding.com hidetopbar@mathieu.bidon.ca rounded-window-corners@fxgn color-picker@tuberry dash-to-panel@jderose9.github.com dash-to-dock@micxgx.gmail.com gtk4-ding@smedius.gitlab.com
-  $EXT_CLI disable background-logo@fedorahosted.org Vitals@CoreCoding.com hidetopbar@mathieu.bidon.ca rounded-window-corners@fxgn color-picker@tuberry dash-to-panel@jderose9.github.com dash-to-dock@micxgx.gmail.com gtk4-ding@smedius.gitlab.com
+  $EXT_CLI install appindicatorsupport@rgcjonas.gmail.com quick-lang-switch@ankostis.gmail.com blur-my-shell@aunetx just-perfection-desktop@just-perfection Vitals@CoreCoding.com hidetopbar@mathieu.bidon.ca rounded-window-corners@fxgn color-picker@tuberry dash-to-panel@jderose9.github.com dash-to-dock@micxgx.gmail.com gtk4-ding@smedius.gitlab.com || throw_err "Error while installing gnome extensions. Try again later"
+  $EXT_CLI disable background-logo@fedorahosted.org Vitals@CoreCoding.com hidetopbar@mathieu.bidon.ca rounded-window-corners@fxgn color-picker@tuberry dash-to-panel@jderose9.github.com dash-to-dock@micxgx.gmail.com gtk4-ding@smedius.gitlab.com || echo "$(warn "Some extensions are not disabled, so you might see some visual issues, disable them, if you need, in Extensions Manager app")"
 } && save_step
+
+step="[12|13]: Setting up look of your desktop"; log_step
+mkdir -p "$WALLPAPERS_DIR" "$PROJECT_DIR/data"
+for f in "${WALLPAPER_FILENAMES[@]}"; do
+  [ -f "$WALLPAPERS_DIR/$f" ] || curl -fsSL "$WALLPAPERS_URL/$f" -o "$WALLPAPERS_DIR/$f" || echo "$(warn "Wallpapers failed to install")"
+done
+curl -fsSL "$RAW_GITHUB/dash-to-panel.conf" -o "$PROJECT_DIR/data/dash-to-panel.conf" || throw_err 'Failed to download "dash-to-panel.conf". Try again later'
 
 SELECTED_LOOK=$(zenity --list --radiolist \
   --title="Desktop Look" \
@@ -168,8 +194,6 @@ SELECTED_LOOK=$(zenity --list --radiolist \
   --column="" --column="Look" \
   FALSE "macos" FALSE "windows" TRUE "linux" \
   --width=480 --height=480)
-
-[ -z "$SELECTED_LOOK" ] && { throw_err "Cancelled"; }
 
 PROGRAMS=$(zenity --list --checklist \
   --title="Programs to install" \
@@ -186,82 +210,99 @@ PROGRAMS=$(zenity --list --checklist \
   FALSE "obs-hotkeys"     "Fix OBS recording hotkeys (you want this if you will record with obs)" \
   --width=960 --height=540)
 
-[ $? -ne 0 ] && { throw_err "Cancelled"; }
-
 selected() { echo "$PROGRAMS" | grep -qw "$1"; }
 
-step="[12|13]: Setting up look of your desktop"; log_step
 case "$SELECTED_LOOK" in
   "windows")
-    WALLPAPER_NAME="windows.jpg"
-    $EXT_CLI install gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com
-    curl -fsSL "$RAW_GITHUB/dash-to-panel.conf" | dconf load /org/gnome/shell/extensions/dash-to-panel/
-    $EXT_CLI enable gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com
-    $EXT_CLI disable dash-to-dock@micxgx.gmail.com hidetopbar@mathieu.bidon.ca
+    WALLPAPER_NAME="${WALLPAPER_FILENAMES[0]}"
+    (
+      set -e
+      $EXT_CLI install gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com
+      cat "$PROJECT_DIR/data/dash-to-panel.conf" | dconf load "$DTP_CONF_PATH"
+      $EXT_CLI enable gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com
+      $EXT_CLI disable dash-to-dock@micxgx.gmail.com hidetopbar@mathieu.bidon.ca
+    ) || echo "$(warn "Failed to set 'windows' style. Try again later")"
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     ;;
   "macos")
-    WALLPAPER_NAME="macos.png"
-    $EXT_CLI install dash-to-dock@micxgx.gmail.com
-    $EXT_CLI enable dash-to-dock@micxgx.gmail.com
-    $EXT_CLI disable gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com
+    WALLPAPER_NAME="${WALLPAPER_FILENAMES[1]}"
+    (
+      set -e
+      $EXT_CLI install dash-to-dock@micxgx.gmail.com
+      $EXT_CLI enable dash-to-dock@micxgx.gmail.com
+      $EXT_CLI disable gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com
+    ) || echo "$(warn "Failed to set 'macos' style. Try again later")"
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     ;;
   "linux")
-    WALLPAPER_NAME="linux.jpg"
-    $EXT_CLI disable gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com dash-to-dock@micxgx.gmail.com
+    WALLPAPER_NAME="${WALLPAPER_FILENAMES[2]}"
+    (
+      set -e
+      $EXT_CLI disable gtk4-ding@smedius.gitlab.com dash-to-panel@jderose9.github.com dash-to-dock@micxgx.gmail.com
+    ) || echo "$(warn "Failed to set 'linux' style. Try again later")"
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
     ;;
 esac
 
-mkdir -p "$WALLPAPERS_DIR"
-if [ ! -f "$WALLPAPERS_DIR/$WALLPAPER_NAME" ]; then
-  curl -fsSL "$WALLPAPERS_URL/$WALLPAPER_NAME" -o "$WALLPAPERS_DIR/$WALLPAPER_NAME"
-fi
-
-WALLPAPER="file://$WALLPAPERS_DIR/$WALLPAPER_NAME"
-gsettings set org.gnome.desktop.background picture-uri "$WALLPAPER"
-gsettings set org.gnome.desktop.background picture-uri-dark "$WALLPAPER"
+[ -z "$SELECTED_LOOK" ] || {
+  WALLPAPER="file://$WALLPAPERS_DIR/$WALLPAPER_NAME"
+  gsettings set org.gnome.desktop.background picture-uri "$WALLPAPER"
+  gsettings set org.gnome.desktop.background picture-uri-dark "$WALLPAPER"
+}
 
 step="[13|13]: Installing recommended programs"; log_step
-if selected "color-picker"; then
-  $EXT_CLI install color-picker@tuberry
-  $EXT_CLI enable color-picker@tuberry
-fi
-if selected "rounded-corners"; then
-  $EXT_CLI install rounded-window-corners@fxgn
-  $EXT_CLI enable rounded-window-corners@fxgn
-fi
-if selected "hidetopbar"; then
-  $EXT_CLI install hidetopbar@mathieu.bidon.ca
-  $EXT_CLI enable hidetopbar@mathieu.bidon.ca
-fi
-if selected "vitals"; then
-  $EXT_CLI install Vitals@CoreCoding.com
-  $EXT_CLI enable Vitals@CoreCoding.com
-fi
+(
+  set -e
+  if selected "color-picker"; then
+    $EXT_CLI install color-picker@tuberry
+    $EXT_CLI enable color-picker@tuberry
+  fi
+  if selected "rounded-corners"; then
+    $EXT_CLI install rounded-window-corners@fxgn
+    $EXT_CLI enable rounded-window-corners@fxgn
+  fi
+  if selected "hidetopbar"; then
+    $EXT_CLI install hidetopbar@mathieu.bidon.ca
+    $EXT_CLI enable hidetopbar@mathieu.bidon.ca
+  fi
+  if selected "vitals"; then
+    $EXT_CLI install Vitals@CoreCoding.com
+    $EXT_CLI enable Vitals@CoreCoding.com
+  fi
+) || echo "$(warn "Some extensions failed to enable. Try again later")"
 
 if selected "minecraft"; then
-  eval "$MC_INSTALLER" || throw_err "Minecraft installation failed. Try later by running this program again"
+  eval "$MC_INSTALLER" || echo "$(warn "Minecraft installation failed. Try later by running this program again")"
 fi
 
 if selected "youtube-music"; then
-  if rpm -qa | grep -q youtube-music; then
+  proceed_ytm_install=true
+  rpm -qa | grep -q youtube-music
+  is_ytm_exists=$?
+
+  [ $is_ytm_exists -eq 0 ] && {
     echo "$(warn "YouTube Music App is already installed")"
-  else
+    ask_confirm "Do you want to reinstall?" || proceed_ytm_install=false
+  }
+
+  if $proceed_ytm_install; then
     ytm_release_url=$(echo "$YTM_DOWNLOAD_URL" | sed 's/api\.//; s/repos\///')
     echo "Installing YouTube Music App from \"$ytm_release_url\"..."
-    curl -s "$YTM_DOWNLOAD_URL" | grep browser_download_url | grep x86_64.rpm | cut -d '"' -f 4 | xargs curl -L -o "$HOME/Downloads/youtube-music.rpm"
-    sudo dnf install -y "$HOME/Downloads/youtube-music.rpm"
+    (
+      set -e
+      [ $is_ytm_exists -eq 0 ] && sudo dnf remove -y youtube-music
+      curl -s "$YTM_DOWNLOAD_URL" | grep browser_download_url | grep x86_64.rpm | cut -d '"' -f 4 | xargs curl -L -o "$HOME/Downloads/youtube-music.rpm"
+      sudo dnf install -y "$HOME/Downloads/youtube-music.rpm"
+    ) || echo "$(warn "Error while installing Youtube Music App. Try again later")"
   fi
 fi
 
 if selected "vicinae"; then
-  eval "$VICINAE_INSTALLER" || throw_err "Vicinae installation failed. Try later by running this program again"
+  eval "$VICINAE_INSTALLER" || echo "$(warn "Vicinae installation failed. Try later by running this program again")"
 fi
 
 if selected "obs-hotkeys"; then
-  eval "$OBS_HOTKEYS_INSTALLER" || throw_err "OBS Hotkeys installation failed. Try later by running this program again"
+  eval "$OBS_HOTKEYS_INSTALLER" || echo "$(warn "OBS Hotkeys installation failed. Try later by running this program again")"
 fi
 
 zenity --info \
